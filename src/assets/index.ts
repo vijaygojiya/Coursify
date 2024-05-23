253593b566f44b1e59e3c3ae620472a0da03bdaf
Vijay Gojiya <vmgojiya32@gmail.com>
2024-05-23 23:32:48 +0530

    feat: add custom fonts and SVG support

---
+++ src/assets/index.ts
@@ -0,0 +1,27 @@
+export {default as Profile} from './svgs/Profile.svg';
+export {default as Chevron} from './svgs/Chevron.svg';
+export {default as EyeHide} from './svgs/HideEye.svg';
+export {default as EyeShow} from './svgs/ShowEye.svg';
+export {default as Lock} from './svgs/Lock.svg';
+export {default as Message} from './svgs/Message.svg';
+export {default as welcome1} from './svgs/welcome-1.svg';
+export {default as welcome2} from './svgs/welcome-2.svg';
+export {default as welcome3} from './svgs/welcome-3.svg';
+export {default as welcome4} from './svgs/welcome-4.svg';
+
+export {default as Menu} from './svgs/menu.svg';
+
+export {default as BookOpenFill} from './svgs/book-open-fill.svg';
+export {default as BookOpenLine} from './svgs/book-open-line.svg';
+export {default as CompassFill} from './svgs/compass-fill.svg';
+export {default as CompassLine} from './svgs/compass-line.svg';
+export {default as SearchLine} from './svgs/search-line.svg';
+export {default as SearchFill} from './svgs/search-fill.svg';
+
+export {default as Genius} from './svgs/genius.svg';
+export {default as IdeaLaunch} from './svgs/idea-launch.svg';
+export {default as ManRidingRocket} from './svgs/man-riding-a-rocket.svg';
+export {default as Presentation} from './svgs/presentation.svg';
+export {default as ProductLaunch} from './svgs/product-launch.svg';
+export {default as Studying} from './svgs/studying.svg';
+export {default as Logout} from './svgs/logout-line.svg';
