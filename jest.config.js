458e5b5002f4c4fd79b6bcfc4e2e6b8787bc650b
Vijay Gojiya <vmgojiya32@gmail.com>
2024-05-24 11:47:52 +0530

    feat:jest and RNTL lib setup in progress

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   preset: 'react-native',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  setupFiles: ['<rootDir>/jest/setup.ts'],
+  moduleNameMapper: {
+    '\\.svg': '<rootDir>/__mocks__/svgMock.js',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|@react-native|@react-native-community|@react-navigation|ky)',
+  ],
+  collectCoverageFrom: [
+    '<rootDir>/src/components/**/*.tsx',
+    '<rootDir>/App.tsx',
+  ],
+  coverageReporters: ['html', 'text', 'text-summary', 'cobertura'],
+  testMatch: ['**/*.test.ts?(x)', '**/*.test.js?(x)'],
 };
